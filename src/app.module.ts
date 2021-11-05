8b314eb0790c65653fa4d0c9778bb02ae8a147f8
zhixiangyao <blueskyzx@icloud.com>
2021-11-05 23:51:58 +0800

    chore: delete and add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { CatsController } from './cats/cats.controller';
+import { CatsService } from './cats/cats.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [CatsController],
+  providers: [CatsService],
 })
-export class AppModule {}
+class AppModule {}
+
+export { AppModule };
