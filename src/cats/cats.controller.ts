6a38ac419873d07362896f24267c55a2305a9d8f
zhixiangyao <blueskyzx@icloud.com>
2021-11-06 00:01:14 +0800

    chore

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,19 @@
 import { Controller } from '@nestjs/common';
+import { Get, Param } from '@nestjs/common';
 
 @Controller('cats')
-export class CatsController {}
+class CatsController {
+  // constructor(private);
+
+  @Get(':id')
+  getCatInfo(@Param('id') id: string): string {
+    return `ID: ${id}`;
+  }
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+}
+
+export { CatsController };
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CatsService {}
+class CatsService {}
+
+export { CatsService };
