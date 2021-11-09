bc10a892af59fee16fee4352acfe7e421a8b2a93
zhixiangyao <blueskyzx@icloud.com>
2021-11-09 16:49:12 +0800

    feat: add gzip compression

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import * as compression from 'compression';
+import type { CompressionFilter } from 'compression';
 
 import { AppModule } from './app.module';
+
+const shouldCompress: CompressionFilter = (req, res) => {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false;
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res);
+};
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: false }));
+  app.use(compression({ filter: shouldCompress }));
+
   await app.listen(3000);
 };
 
